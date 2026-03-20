8132e82fef07c6d0075bb765b331ccd29a7d0156
Smart Grocer Developer <dev@smartgrocer.local>
2026-03-21 00:58:03 +0530

    ititial commit

--- core/models.py
+++ core/models.py
@@ -3,11 +3,22 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class Platform(models.Model):
     name = models.CharField(max_length=100)
     link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
 
 class Price(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
     price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.product.name} - {self.platform.name} - ₹{self.price}"
